13b30e324e145d62b73022d4b9829c2e6f4d14ff
Thomas Jessop <tljessop@hotmail.com>
2018-05-11 21:38:13 -0700

    updated version of ical script that can now dynamical write the event start time. p.s. I should have commited this many edits ago it's majorly diffrent from the last commit

--- ical_events.sh
+++ ical_events.sh
@@ -11,7 +11,7 @@ ucal_file=$2
 #end of var
 
 #wellcome message
-echo "Wellcome to $(basename $0) your terminal calandar event wisard"
+echo "Wellcome to $(basename $0) your terminal calandar event wizard"
 
 error(){
 	echo "$(basename $0): Error your first argument must be -c or -r"
@@ -34,39 +34,85 @@ elif [ $1 != "-c" -a $1 != "-r" ]; then
 	echo "That was not a valid option: You input $1"
 	error
 
+#create mode path
 elif [ "$1" = "-c" ]; then
 	echo "create mode"
-
+#Read mode path 
 elif [ "$1" = "-r" ]; then
 	if [ $# -lt 2 ]; then 
 		error	
 	elif [ ! -e $2 ]; then 
+		echo "Error: $2 doesn't exist"
 		error
 	elif [ ! -r $2 ]; then
 		echo "$2 must be readable"
 		exit 1
-	elif [ "$2" != '*.ical' ]; then
-		echo "Error: your second arguments was $2"
+
+	elif ! file $2 | grep "vCalendar" >/dev/null 2>/dev/null; then
+		echo "Error: your second arguments was not a valid calendar file"
+		echo "Your second argument was $2"
 		error  
 	fi
-
 	echo "read mode"
 fi
+#End of Input verification
 
+#Mode Branching
+if [ "$1" = "-c" ]; then
+	#reads in start date
+	echo "In what on date does your event start?"
+	read stdate
+	until [[ "$stdate" = [0-1][[:digit:]]/[0-3][[:digit:]]/[[:digit:]][[:digit:]][[:digit:]][[:digit:]] ]]; do
+		echo "Sorry that's not not the right format"
+		echo "You gave $stdate as the date"
+		echo "$(basename $0) accepts date in the month/day/year format. ie 07/04/1776"
+		read stdate
+	done 
 
-#writes file to be exported
+	#reformats the date to match the .ical spec
+	stdate=$( echo "$stdate" | sed -E 's_([[:digit:]]+/)([[:digit:]]+/)([[:digit:]]+)_\3\2\1_' | sed -E 's_/__g')
 
+	#reads in start time
+	echo "What time does your event start?"
+	read stime
+	until [[ "$stime" = [0-1][[:digit:]]:[0-5][[:digit:]]am ]] || [[ "$stime" = [0-1][[:digit:]]:[0-5][[:digit:]]pm ]]; do
+		echo "Sorry that's not not the right format"
+		echo "You gave $stime as the time"
+		echo "$(basename $0) accepts time in the hour:minute am or pm format. ie 07:45am or 05:20pm"
+		read stime
+	done 
+	#checks to see if start time is am or pm
+	if [[ "$stime" = *am ]]; then
+		#formats input time for am input
+		stime=$(echo $stime | sed -E 's/([[:digit:]]+:)([[:digit:]]+)(.+)/T\1\200/' | tr -d : )
+	elif [[ "$stime" = *pm ]]; then
+		#cuts the hour value off and adds 12 to convert to military time
+		conver_hour=$(( ${stime:0:2} + 12))
+		#Prevents the hour reading out at 24 
+		if [ $conver_hour -eq 24 ]; then
+			conver_hour=00
+		fi
+		#adds back the converted hour and formats the string
+		stime=$(echo T${conver_hour}$(echo $stime | sed -E 's/([[:digit:]]+:)([[:digit:]]+)(.+)/\200/' | tr -d :) )
+	fi
+	
+	#Merges start date and start time which is needed for proper formating
+	stdate=$(echo ${stdate}${stime})
+	echo $stdate
+fi
 echo "BEGIN:VCALENDAR" >>cal_test.ical
-#echo "VERSION:2.0" >>cal_test.ical
-#echo "PRODID:-//tljessop.com" >>cal_test.ical
-#echo "METHOD:PUBLISH" >>cal_test.ical
-#echo "BEGIN:VEVENT" >>cal_test.ical
-#echo "SUMMARY:Abraham Lincoln" >>cal_test.ical
-#echo "UID:2008-04-2" >>cal_test.ical
-#echo "DTSTART:20080212" >>cal_test.ical
-#echo "DTEND:20080213" >>cal_test.ical
-#echo "DTSTAMP:20150421T141403" >>cal_test.ical
-#echo "LOCATION:Hodgenville\, Kentucky" >>cal_test.ical
-#echo "DESCRIPTION:Born February 12\, 1809" >>cal_test.ical
-#echo "END:VEVENT" >>cal_test.ical
-#echo "END:VCALENDAR" >>cal_test.ical
+echo "VERSION:2.0" >>cal_test.ical
+echo "PRODID:-//tljessop.com" >>cal_test.ical
+echo "METHOD:PUBLISH" >>cal_test.ical
+echo "BEGIN:VEVENT" >>cal_test.ical
+echo "SUMMARY:Abraham Lincoln" >>cal_test.ical
+echo "UID:2008-04-2" >>cal_test.ical
+echo "DTSTART:$stdate" >>cal_test.ical
+echo "DTEND:20080213" >>cal_test.ical
+echo "DTSTAMP:20150421T141403" >>cal_test.ical
+echo "LOCATION:Hodgenville\, Kentucky" >>cal_test.ical
+echo "DESCRIPTION:Born February 12\, 1809" >>cal_test.ical
+echo "END:VEVENT" >>cal_test.ical
+echo "END:VCALENDAR" >>cal_test.ical
+
+
